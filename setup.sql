7d22add45560e1e9e511fb24d72882987df97711
Chino Rodriguez <chino@Chinos-MacBook-Pro.local>
2022-06-25 19:03:38 -0700

    implemented auth, see notes.txt and setup.sql

--- setup.sql
+++ setup.sql
@@ -2,19 +2,24 @@
 --CREATE TYPE muscleGroup AS ENUM ('chest', 'shoulders', 'biceps', 'triceps', 'traps', 'lats', 'lower_back', 'abs', 'hamstrings', 'quads', 'glutes', 'calves');
 
 --DROP TABLE set1;
--- CREATE TABLE IF NOT EXISTS SET1 (reps INT NOT NULL,
---                   weight INT NOT NULL,
---                   date DATE NOT NULL,
---                   exercise EXERCISE NOT NULL,
---                   musclegroup MUSCLEGROUP NOT NULL,
---                   comments varchar(40) DEFAULT '');
+ --CREATE TABLE IF NOT EXISTS SET1 (reps INT NOT NULL,
+--                    weight INT NOT NULL,
+--                    date DATE NOT NULL,
+--                    exercise EXERCISE NOT NULL,
+--                    musclegroup MUSCLEGROUP NOT NULL,
+--                    comments varchar(40) DEFAULT '',
+--                    owner UUID NOT NULL,
+--                    CONSTRAINT fk_owner
+--                                    FOREIGN KEY(owner)
+--                                   REFERENCES appUser(id)
+-- );
 
 
 --SELECT enum_range(NULL::exercise); -- view exercise enum
 --SELECT enum_range(NULL::muscleGroup); -- view muscleGroup enum
 
--- CREATE TABLE user(
---     id bigserial PRIMARY KEY,
---     username varchar(40) NOT NULL UNIQUE,
---     password varchar(100)
+-- CREATE TABLE appUser(
+--     id UUID PRIMARY KEY,
+--     username varchar(30) NOT NULL UNIQUE,
+--     password varchar(60)
 --                  )
