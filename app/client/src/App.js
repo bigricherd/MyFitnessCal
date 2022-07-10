e4e062823b0543b8777331913e1e2ba3216ddfe3
Tricia Faith Cu <triciafaithcu@gmail.com>
2022-07-09 20:52:02 -0700

    feat: :construction: added backend endpoint route for getting and adding sessions

--- app/client/src/App.js
+++ app/client/src/App.js
@@ -1,25 +1,24 @@
-import React, { useState, useEffect, useCallback } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Register from './components/pages/Register';
-import Login from './components/pages/Login';
-import Forms from './components/pages/Forms';
-import HomePage from './components/pages/HomePage';
-import Nav from './components/Nav';
-import Filters from './components/pages/Filters';
+import React, { useState, useEffect, useCallback } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Register from "./components/pages/Register";
+import Login from "./components/pages/Login";
+import Forms from "./components/pages/Forms";
+import HomePage from "./components/pages/HomePage";
+import Nav from "./components/Nav";
+import Filters from "./components/pages/Filters";
 
 function App() {
-  const baseUrl = process.env.REACT_APP_HOME_URL || 'http://localhost:5000';
+  const baseUrl = process.env.REACT_APP_HOME_URL || "http://localhost:5000";
 
   const [message, setMessage] = useState(null);
   const [user, setUser] = useState(null);
   const [isFetching, setIsFetching] = useState(false);
-  const url = `${baseUrl}/api/auth/getUser`
+  const url = `${baseUrl}/api/auth/getUser`;
   console.log(url);
 
   const fetchUser = useCallback(async () => {
-
     const response = await fetch(url, { credentials: "include" });
     if (!response.ok) {
       throw new Error(`status ${response.status}`);
@@ -33,7 +32,6 @@ function App() {
       setMessage(`API call failed: ${e}`);
       setIsFetching(false);
     }
-
   }, [url]);
 
   useEffect(() => {
@@ -41,36 +39,37 @@ function App() {
     fetchUser();
   }, [fetchUser]);
 
-  const debugText = <div>
-    <p>{'« '}<strong>
-      {isFetching
-        ? 'Fetching user '
-        : message}
-    </strong>{' »'}
-    </p>
-    {process.env.NODE_ENV === 'production' ?
+  const debugText = (
+    <div>
       <p>
-        This is a production build.
+        {"« "}
+        <strong>{isFetching ? "Fetching user " : message}</strong>
+        {" »"}
       </p>
-      : <p>
-        You're not on PROD.
-      </p>
-    }</div>;
+      {process.env.NODE_ENV === "production" ? (
+        <p>This is a production build.</p>
+      ) : (
+        <p>You're not on PROD.</p>
+      )}
+    </div>
+  );
 
   return (
     <Router>
       <div className="App">
         <header className="App-header">
           <Nav user={user} />
+        </header>
+        <body className="App-body">
           <Routes>
-            <Route exact path='/' element={<HomePage user={user} />} />
-            <Route exact path='/forms' element={<Forms user={user} />} />
-            <Route exact path='/filters' element={<Filters user={user} />} />
-            <Route exact path='/register' element={<Register />} />
-            <Route exact path='/login' element={<Login />} />
+            <Route exact path="/" element={<HomePage user={user} />} />
+            <Route exact path="/forms" element={<Forms user={user} />} />
+            <Route exact path="/filters" element={<Filters user={user} />} />
+            <Route exact path="/register" element={<Register />} />
+            <Route exact path="/login" element={<Login />} />
           </Routes>
           {debugText}
-        </header>
+        </body>
       </div>
     </Router>
   );
