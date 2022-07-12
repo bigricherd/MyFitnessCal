5d5e8b1997bbcc5fa68c353085c6becd84b32bf0
Chino Rodriguez <chino@Chinos-MacBook-Pro.local>
2022-07-11 20:40:58 -0700

    feature: confirm delete exercise dialog

--- app/client/src/components/ManageExercisesList.jsx
+++ app/client/src/components/ManageExercisesList.jsx
@@ -1,19 +1,37 @@
 import React, { useState, useEffect, useCallback } from 'react';
 import formatEnum from '../helpers/formatEnum';
-import { Grid, Button, Box } from '@mui/material';
+import { Grid, Button, Box, Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions } from '@mui/material';
 import useForm from '../hooks/useDeleteForm'
 
 function ManageExercisesList(props) {
+
+    // Two state variables to store index and event corresponding to 
+    const [indexToDelete, setIndexToDelete] = useState(null);
+    const [deleteEvent, setDeleteEvent] = useState(null);
+    const [open, setOpen] = useState(false);
+    const handleClickOpen = (e, i) => {
+        e.persist();
+        setOpen(true);
+        setIndexToDelete(i);
+        setDeleteEvent(e);
+    };
+    const handleClose = () => {
+        console.log(`Selected exercises was at index ${indexToDelete}, now setting to null`);
+        setIndexToDelete(null);
+        setDeleteEvent(null);
+        setOpen(false);
+    }
+
     const { values, handleSubmit, error, successMsg, exercisesPostDelete } = useForm(
         { exercise: '' }
     )
 
-    const onDeleteClick = async (e, index) => {
-        e.persist();
-        const source = props.exercisesByUser;
-        values.exercise = source[index];
-        handleSubmit(e);
+    const handleConfirmDelete = () => {
+        // console.log(`I am supposed to delete exercise at index ${indexToDelete}. Corresponding event below`);
+        values.exercise = props.exercisesByUser[indexToDelete];
+        handleSubmit(deleteEvent);
         values.exercise = '';
+        setOpen(false);
     }
 
     const liftState = useCallback(() => {
@@ -26,7 +44,7 @@ function ManageExercisesList(props) {
                 {item}
             </Box>
 
-            <Button fullWidth variant="contained" color='success' onClick={(e) => onDeleteClick(e, index)}>
+            <Button fullWidth variant="contained" color='success' onClick={(e) => handleClickOpen(e, index)}>
                 <Grid item xs={3} sm={2}>X</Grid>
             </Button>
         </Grid >
@@ -38,8 +56,9 @@ function ManageExercisesList(props) {
                 <Box component={Grid} item xs={9} sm={10} bgcolor={'white'} color={'gray'}>
                     {item}
                 </Box>
+
                 <Grid item xs={3} sm={2}>
-                    <Button variant="contained" onClick={(e) => onDeleteClick(e, index)}>X</Button>
+                    <Button variant="contained" onClick={(e) => handleClickOpen(e, index)}>X</Button>
                 </Grid>
             </Grid >
         ));
@@ -52,11 +71,31 @@ function ManageExercisesList(props) {
 
     return (
         <Grid item xs={10} sm={8}>
+
+            {/* Confirm delete dialog, triggered when a user clicks the 'X' button corresponding to some exercise */}
+            <Dialog
+                open={open}
+                onClose={handleClose}
+            >
+                <DialogTitle>Delete exercise {props.exercisesByUser[indexToDelete]}?</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        This cannot be undone.
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose}>Back</Button>
+                    <Button onClick={handleConfirmDelete} sx={{ color: 'red' }}>Delete</Button>
+                </DialogActions>
+            </Dialog>
+
+            {/* Main component content: manage exercises list */}
             <h1>Manage Exercises</h1>
             <div className="manageExercisesList">
                 {list}
             </div>
-            {/**Doesn't show right now */}
+
+            {/* Feedback messages */}
             {successMsg && <h3>{successMsg}</h3>}
             {error && <h3>{error}</h3>}
         </Grid>
