963f58044c7fd3d22598b0f011abba9b8bcd9384
Chino Rodriguez <chino@Chinos-MacBook-Pro.local>
2022-07-12 19:01:47 -0700

    now using Box instead of Grid to center Register and Login forms. Import statement cleanup

--- app/client/src/components/pages/Login.jsx
+++ app/client/src/components/pages/Login.jsx
@@ -1,6 +1,17 @@
 import React, { useState } from 'react';
 import useForm from '../../hooks/useAuthForm';
-import { Typography, Grid, Stack, Container, Button, FormControl, FormLabel, InputAdornment, IconButton, TextField } from '@mui/material';
+import {
+    Box,
+    Container,
+    Typography,
+    Stack,
+    FormControl,
+    FormLabel,
+    TextField,
+    InputAdornment,
+    IconButton,
+    Button
+} from '@mui/material';
 import { Visibility, VisibilityOff } from '@mui/icons-material';
 //import Error from './Error'; //TODO: show error message on incorrect password, invalid username
 
@@ -21,62 +32,57 @@ function Login() {
     });
 
     return (
-        <Grid
-            container
-            spacing={0}
-            direction="column"
-            alignItems="center"
+        <Box
+            display="flex"
             justifyContent="center"
-        >
-            <Grid item xs={3}>
-                <Container>
-                    <Stack spacing={2}>
-                        <Typography variant="h3" gutterBottom>
-                            Login
-                        </Typography>
-                        <FormControl>
-                            <FormLabel>Username</FormLabel>
-                            <TextField
-                                name="username"
-                                value={values.username}
-                                onChange={handleChange}
-                                onKeyDown={handleKeyDown}
-                                required >
-                            </TextField>
-                        </FormControl>
+            alignItems="center"
+            minHeight="100vh">
+            <Container>
+                <Typography variant="h3" gutterBottom>
+                    Login
+                </Typography>
+                <Stack spacing={2}>
+                    <FormControl>
+                        <FormLabel>Username</FormLabel>
+                        <TextField
+                            name="username"
+                            value={values.username}
+                            onChange={handleChange}
+                            onKeyDown={handleKeyDown}
+                            required >
+                        </TextField>
+                    </FormControl>
 
-                        <FormControl>
-                            <FormLabel>Password</FormLabel>
-                            <TextField
-                                name="password"
-                                type={showPassword ? 'text' : 'password'}
-                                value={values.password}
-                                onChange={handleChange}
-                                onKeyDown={handleKeyDown}
-                                InputProps={{
-                                    endAdornment: <InputAdornment position="end">
-                                        <IconButton
-                                            onClick={handleClickShowPassword}
-                                        >
-                                            {showPassword ? <VisibilityOff /> : <Visibility />}
-                                        </IconButton>
-                                    </InputAdornment>
-                                }}
-                                required>
-                            </TextField>
-                        </FormControl>
-                    </Stack>
-                    <Button
-                        onClick={handleSubmit}
-                        type="submit"
-                        color="primary"
-                        variant="contained"
-                    > Login
-                    </Button>
-                </Container>
-            </Grid>
-        </Grid>
-
+                    <FormControl>
+                        <FormLabel>Password</FormLabel>
+                        <TextField
+                            name="password"
+                            type={showPassword ? 'text' : 'password'}
+                            value={values.password}
+                            onChange={handleChange}
+                            onKeyDown={handleKeyDown}
+                            InputProps={{
+                                endAdornment: <InputAdornment position="end">
+                                    <IconButton
+                                        onClick={handleClickShowPassword}
+                                    >
+                                        {showPassword ? <VisibilityOff /> : <Visibility />}
+                                    </IconButton>
+                                </InputAdornment>
+                            }}
+                            required>
+                        </TextField>
+                    </FormControl>
+                </Stack>
+                <Button
+                    onClick={handleSubmit}
+                    type="submit"
+                    color="primary"
+                    variant="contained"
+                > Login
+                </Button>
+            </Container>
+        </Box>
     )
 }
 
--- app/client/src/components/pages/Register.jsx
+++ app/client/src/components/pages/Register.jsx
@@ -1,6 +1,17 @@
 import React, { useState } from 'react';
 import useForm from '../../hooks/useAuthForm';
-import { Typography, Grid, Stack, Container, Button, FormControl, FormLabel, InputAdornment, IconButton, TextField } from '@mui/material';
+import {
+    Box,
+    Container,
+    Typography,
+    Stack,
+    FormControl,
+    FormLabel,
+    TextField,
+    InputAdornment,
+    IconButton,
+    Button
+} from '@mui/material';
 import { Visibility, VisibilityOff } from '@mui/icons-material';
 //import Error from './Error'; //TODO: show error message on duplicate username, password too short(?)
 
@@ -22,61 +33,56 @@ function Register() {
 
     return (
 
-        <Grid
-            container
-            spacing={0}
-            direction="column"
-            alignItems="center"
+        <Box
+            display="flex"
             justifyContent="center"
-            style={{ minHeight: '100vh' }}
-        >
-            <Grid item xs={3}>
-                <Container component="div">
-                    <Typography variant="h3" gutterBottom>
-                        Register
-                    </Typography>
-                    <Stack spacing={2}>
-                        <FormControl>
-                            <FormLabel>Username</FormLabel>
-                            <TextField
-                                name="username"
-                                value={values.username}
-                                onChange={handleChange}
-                                onKeyDown={handleKeyDown}
-                                required >
-                            </TextField>
-                        </FormControl>
-                        <FormControl>
-                            <FormLabel>Password</FormLabel>
-                            <TextField
-                                name="password"
-                                type={showPassword ? 'text' : 'password'}
-                                value={values.password}
-                                onChange={handleChange}
-                                onKeyDown={handleKeyDown}
-                                InputProps={{
-                                    endAdornment:
-                                        <InputAdornment position="end">
-                                            <IconButton
-                                                onClick={handleClickShowPassword}
-                                            > {showPassword ? <VisibilityOff /> : <Visibility />}
-                                            </IconButton>
-                                        </InputAdornment>
-                                }}
-                                required>
-                            </TextField>
-                        </FormControl>
-                    </Stack>
-                    <Button
-                        onClick={handleSubmit}
-                        type="submit"
-                        color="primary"
-                        variant="contained"
-                    > Register
-                    </Button>
-                </Container>
-            </Grid>
-        </Grid>
+            alignItems="center"
+            minHeight="100vh">
+            <Container component="div">
+                <Typography variant="h3" gutterBottom>
+                    Register
+                </Typography>
+                <Stack spacing={2}>
+                    <FormControl>
+                        <FormLabel>Username</FormLabel>
+                        <TextField
+                            name="username"
+                            value={values.username}
+                            onChange={handleChange}
+                            onKeyDown={handleKeyDown}
+                            required >
+                        </TextField>
+                    </FormControl>
+                    <FormControl>
+                        <FormLabel>Password</FormLabel>
+                        <TextField
+                            name="password"
+                            type={showPassword ? 'text' : 'password'}
+                            value={values.password}
+                            onChange={handleChange}
+                            onKeyDown={handleKeyDown}
+                            InputProps={{
+                                endAdornment:
+                                    <InputAdornment position="end">
+                                        <IconButton
+                                            onClick={handleClickShowPassword}
+                                        > {showPassword ? <VisibilityOff /> : <Visibility />}
+                                        </IconButton>
+                                    </InputAdornment>
+                            }}
+                            required>
+                        </TextField>
+                    </FormControl>
+                </Stack>
+                <Button
+                    onClick={handleSubmit}
+                    type="submit"
+                    color="primary"
+                    variant="contained"
+                > Register
+                </Button>
+            </Container>
+        </Box>
     )
 }
 
