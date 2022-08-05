2dd914694d973ab91f94619aa6409d4fe38f0f94
Chino Rodriguez <chino@Chinos-MacBook-Pro.local>
2022-08-05 15:08:21 -0700

    added docs for ManageExercisesList. component is messy right now due to Edit Exercise code that is commented out

--- app/client/src/components/ManageExercisesList.jsx
+++ app/client/src/components/ManageExercisesList.jsx
@@ -9,24 +9,36 @@ import {
     DialogContent,
     DialogContentText,
     DialogActions,
-    Alert
+    Alert,
+    Typography,
+    FormControl,
+    FormLabel,
+    TextField
 } from '@mui/material';
-import useForm from '../hooks/useDeleteForm'
+import Dropdown from './Dropdown';
+import useForm from '../hooks/useDeleteForm';
+import editExercise from '../hooks/editExercise';
 
 function ManageExercisesList(props) {
 
-    // Two state variables to store index and event corresponding to 
+    // ------ DELETE EXERCISE ------
+
+    // Two state variables to store index and event corresponding to the exercise in the Confirm Delete dialog
     const [indexToDelete, setIndexToDelete] = useState(null);
     const [deleteEvent, setDeleteEvent] = useState(null);
 
-    // State variable and handlers for the confirm delete dialog
+    // State variable that represents visibility of Confirm Delete dialog
     const [open, setOpen] = useState(false);
+
+    // Open Confirm Delete dialog handler
     const handleClickOpen = (e, i) => {
         e.persist();
         setOpen(true);
         setIndexToDelete(i);
         setDeleteEvent(e);
     };
+
+    // Close Confirm Delete dialog handler
     const handleClose = () => {
         console.log(`Selected exercises was at index ${indexToDelete}, now setting to null`);
         setIndexToDelete(null);
@@ -34,7 +46,7 @@ function ManageExercisesList(props) {
         setOpen(false);
     }
 
-    // Delete form hook 
+    // Delete form hook and handler
     const { values, handleSubmit, error, prevError, successMsg, prevSuccessMsg, exercisesPostDelete } = useForm(
         { exercise: '' }
     )
@@ -47,10 +59,55 @@ function ManageExercisesList(props) {
         setOpen(false);
     }
 
+    // ------ EDIT EXERCISE ------
+
+    // Edit form hook and handler
+    // const { editValues, handleEdit, handleChangeEdit, handleKeyDownEdit, exercisesPostEdit, editError, prevEditError, editSuccessMsg, prevEditSuccessMsg } = editExercise(
+    //     {
+    //         initialValues: {
+    //             exercise: '',
+    //             newName: '',
+    //             newMuscleGroup: ''
+    //         }
+    //     }
+    // )
+
+    // const customHandleEdit = (e) => {
+    //     editValues.exercise = props.exercisesByUser[indexOfEditing];
+    //     handleEdit(e);
+    //     handleCloseEdit();
+    // }
+
+    // // State variable that represents visibility of 'Edit' dialog; handlers follow
+    // const [editing, setEditing] = useState(false);
+    // const [indexOfEditing, setIndexOfEditing] = useState(null);
+    // const handleClickEdit = (e, i) => {
+    //     e.persist();
+    //     editValues.newName = props.exercisesByUser[i];
+    //     console.log(editValues);
+    //     setIndexOfEditing(i);
+    //     setEditing(true);
+    //     console.log(`Going to show edit dialog for item at index ${i}, named ${props.exercisesByUser[i]}`);
+    // }
+
+    // const handleCloseEdit = () => {
+    //     // editValues.exercise = '';
+    //     // editValues.newName = '';
+    //     // editValues.newMuscleGroup = '';
+    //     setEditing(false);
+    //     setIndexOfEditing(null);
+    //     console.log('Setting index of editing to null');
+    // }
+
+
+    // ------ LIST OF EXERCISES ------
+
+    // Function that lifts updated list of exercises to the parent component -- pages/Exercises.jsx
     const liftState = useCallback(() => {
         props.liftState(formatEnum(exercisesPostDelete));
     }, [exercisesPostDelete])
 
+    // State variable that represents the list of exercises created by the current user
     const [list, setList] = useState(props.exercisesByUser.map((item, index) =>
         <Grid container key={index} className="manageExercisesListItem">
             <Box component={Grid} item xs={9} sm={10} bgcolor={'white'} color={'gray'}>
@@ -66,15 +123,14 @@ function ManageExercisesList(props) {
     useEffect(() => {
         setList(props.exercisesByUser.map((item, index) =>
             <Grid container key={index} className="manageExercisesListItem" >
-                <Box component={Grid} item xs={9} sm={10} bgcolor={'white'} color={'gray'}>
+                <Box component={Grid} item xs={7} sm={8} bgcolor={'white'} color={'gray'}>
                     {item}
                 </Box>
-
-                <Grid item xs={3} sm={2}>
-                    <Button variant="contained" onClick={(e) => handleClickOpen(e, index)}>X</Button>
-                </Grid>
+                {/* <Button component={Grid} item xs={3} sm={2} fullWidth variant="contained" color='info' onClick={(e) => handleClickEdit(e, index)}>Edit</Button> */}
+                <Button component={Grid} item xs={2} variant="contained" onClick={(e) => handleClickOpen(e, index)}>X</Button>
             </Grid >
         ));
+        // setMuscleGroups(props.muscleGroups);
     }, [props]); // React complains that onDeleteClick is a missing dependency, but adding it results in "maximum update depth exceeded"
 
     useEffect(() => {
@@ -82,22 +138,20 @@ function ManageExercisesList(props) {
         liftState();
     }, [exercisesPostDelete, liftState]);
 
-    // User feedback -- success and error
-    const [showSuccessMsg, setShowSuccessMsg] = useState(false);
 
-    const handleCloseSuccessMsg = () => {
-        setShowSuccessMsg(false);
-    }
+    // ----- FEEDBACK MESSAGES ------
+
+    // Setup to show feedback message -- success
+    const [showSuccessMsg, setShowSuccessMsg] = useState(false);
+    const handleCloseSuccessMsg = () => { setShowSuccessMsg(false); }
 
     useEffect(() => {
         if (successMsg) setShowSuccessMsg(true);
     }, [successMsg, prevSuccessMsg])
 
+    // Setup to show feedback message -- error
     const [showError, setShowError] = useState(false);
-
-    const handleCloseError = () => {
-        setShowError(false);
-    }
+    const handleCloseError = () => { setShowError(false); }
 
     useEffect(() => {
         if (error) setShowError(true);
@@ -106,7 +160,7 @@ function ManageExercisesList(props) {
     return (
         <Grid item xs={10} sm={8}>
 
-            {/* Confirm delete dialog, triggered when a user clicks the 'X' button corresponding to some exercise */}
+            {/* Confirm delete dialog, which opens when a user clicks the 'X' button corresponding to some exercise */}
             <Dialog
                 open={open}
                 onClose={handleClose}
@@ -123,8 +177,53 @@ function ManageExercisesList(props) {
                 </DialogActions>
             </Dialog>
 
-            {/* Main component content: manage exercises list */}
-            <h1>Manage Exercises</h1>
+            {/* Edit dialog */}
+            {/* <Dialog
+                open={editing}
+                onClose={handleCloseEdit}
+            >
+                <DialogTitle>Edit exercise {props.exercisesByUser[indexOfEditing]}</DialogTitle>
+                <DialogContent>
+                    <Box component="form" onSubmit={(e) => customHandleEdit(e)}>
+                        <FormControl fullWidth>
+                            <FormLabel>Name</FormLabel>
+                            <TextField
+                                name="newName"
+                                id="newName"
+                                value={props.exercisesByUser[indexOfEditing]}
+                                onChange={handleChangeEdit}
+                                onKeyDown={handleKeyDownEdit}
+                            ></TextField>
+                        </FormControl>
+
+                        <FormControl fullWidth>
+                            <FormLabel>Muscle Group</FormLabel>
+                            <Dropdown
+                                name="newMuscleGroup"
+                                id="newMuscleGroup"
+                                options={muscleGroups}
+                                value={editValues.newMuscleGroup}
+                                onChange={handleChangeEdit}
+                                onKeyDown={handleKeyDownEdit}
+                            />
+                        </FormControl>
+                        <Button
+                            type="submit"
+                            variant="contained"
+                            onClick={(e) => { customHandleEdit(e) }}
+                        >Save Changes</Button>
+                    </Box>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleCloseEdit}>Back</Button>
+                </DialogActions>
+            </Dialog> */}
+
+            {/* List of exercises (main component content) */}
+            <Typography variant="h3" gutterBottom>
+                Manage Exercises
+            </Typography>
+
             <div className="manageExercisesList">
                 {list}
             </div>
