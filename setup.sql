d9ebee4c7cf2f9f1f587a462459fdc1fe5ab494c
Big Richard <bigrichard69loo@gmail.com>
2022-08-30 20:57:55 -0700

    DB setup change: renamed set1 to set

--- setup.sql
+++ setup.sql
@@ -42,26 +42,46 @@
 -- select * from session
 
 -- DROP TABLE set1;
---  CREATE TABLE IF NOT EXISTS SET1 (
+--  CREATE TABLE IF NOT EXISTS SET (
 --                    id UUID NOT NULL,
 --                    reps INT NOT NULL,
 --                    weight INT NOT NULL,
 --                    date DATE NOT NULL,
 --                    exercise varchar(45) NOT NULL,
 --                                   CONSTRAINT fk_exercise
---                                     FOREIGN KEY(exercise)
---                                   REFERENCES exercises(nameandmusclegroup)
---                                   ON DELETE CASCADE,
+--                                      FOREIGN KEY(exercise)
+--                                      REFERENCES exercises(nameandmusclegroup)
+--                                      ON DELETE CASCADE,
 --                    musclegroup MUSCLEGROUP NOT NULL,
 --                    owner UUID NOT NULL,
---                    CONSTRAINT fk_owner
---                                    FOREIGN KEY(owner)
---                                   REFERENCES appUser(id)
---                                  session UUID NOT NULL,
---                                    CONSTRAINT fk_session
---                                    FOREIGN KEY(session)
---                                   REFERENCES session(id)
---                                   ON DELETE CASCADE
+--                                  CONSTRAINT fk_owner
+--                                      FOREIGN KEY(owner)
+--                                      REFERENCES appUser(id)
+--                                      ON DELETE CASCADE,
+--                    session UUID NOT NULL,
+--                                  CONSTRAINT fk_session
+--                                      FOREIGN KEY(session)
+--                                      REFERENCES session(id)
+--                                      ON DELETE CASCADE
 -- );
 
--- SELECT * FROM set1
+-- For testing sets alone, while Sets + Sessions integration is pending
+--  CREATE TABLE IF NOT EXISTS SET (
+--                    id UUID NOT NULL,
+--                    reps INT NOT NULL,
+--                    weight INT NOT NULL,
+--                    date DATE NOT NULL,
+--                    exercise varchar(45) NOT NULL,
+--                                   CONSTRAINT fk_exercise
+--                                      FOREIGN KEY(exercise)
+--                                      REFERENCES exercises(nameandmusclegroup)
+--                                      ON DELETE CASCADE,
+--                    musclegroup MUSCLEGROUP NOT NULL,
+--                    owner UUID NOT NULL,
+--                                   CONSTRAINT fk_owner
+--                                      FOREIGN KEY(owner)
+--                                      REFERENCES appUser(id)
+--                                      ON DELETE CASCADE
+-- );
+
+-- SELECT * FROM set
